c46162bced3a458980316f68e79e9780684b3940
Rubicones <dmitriypopov1543@gmail.com>
2025-04-29 01:12:44 +0200

    init

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+import { cn } from "@/lib/utils";
+
+export default function Card({
+    word,
+    translation,
+    classes = "",
+    isTranslation = false,
+}: {
+    word: string;
+    translation: string;
+    classes?: string;
+    isTranslation?: boolean;
+}) {
+    const wordSpan = useRef<HTMLSpanElement>(null);
+    const translationSpan = useRef<HTMLSpanElement>(null);
+    const opacityOut = useRef<Animation | null>(null);
+    const blurOut = useRef<Animation | null>(null);
+    const opacityIn = useRef<Animation | null>(null);
+    const blurIn = useRef<Animation | null>(null);
+    const rotationDirection = useRef(Math.random() > 0.5 ? "-" : "");
+    const rotationAngle = useRef(Math.floor(Math.random() * 5));
+    const flipNumber = useRef(0);
+
+    const flipCard = () => {
+        console.log("Next visible: ", isTranslation ? word : translation);
+
+        const opacityOutKeyframes = new KeyframeEffect(
+            isTranslation ? translationSpan.current : wordSpan.current,
+            [{ opacity: 1 }, { opacity: 0 }],
+            {
+                duration: 500,
+                fill: "forwards",
+            }
+        );
+
+        const blurOutKeyframes = new KeyframeEffect(
+            isTranslation ? translationSpan.current : wordSpan.current,
+            [{ filter: "blur(0px)" }, { filter: "blur(10px)" }],
+            {
+                duration: 500,
+                fill: "forwards",
+            }
+        );
+
+        const opacityInKeyframes = new KeyframeEffect(
+            !isTranslation ? translationSpan.current : wordSpan.current,
+            [{ opacity: 0 }, { opacity: 1 }],
+            {
+                duration: 500,
+                fill: "forwards",
+            }
+        );
+
+        const blurInKeyframes = new KeyframeEffect(
+            !isTranslation ? translationSpan.current : wordSpan.current,
+            [{ filter: "blur(10px)" }, { filter: "blur(0)" }],
+            {
+                duration: 500,
+                fill: "forwards",
+            }
+        );
+        opacityOut.current = new Animation(
+            opacityOutKeyframes,
+            document.timeline
+        );
+
+        blurOut.current = new Animation(blurOutKeyframes, document.timeline);
+
+        opacityIn.current = new Animation(
+            opacityInKeyframes,
+            document.timeline
+        );
+        blurIn.current = new Animation(blurInKeyframes, document.timeline);
+    };
+    useEffect(() => {
+        if (wordSpan.current && translationSpan.current) {
+            flipCard();
+        }
+    }, [word]);
+
+    useEffect(() => {
+        if (flipNumber.current !== 0 && blurOut.current) {
+            translationSpan.current?.classList.remove("temporarilyHidden");
+            opacityIn.current?.play();
+            blurIn.current?.play();
+            opacityOut.current?.play();
+            blurOut.current?.play();
+
+            blurOut.current.onfinish = () => {
+                flipCard();
+            };
+        }
+        flipNumber.current = 1;
+    }, [isTranslation]);
+    return (
+        <>
+            <div
+                className={cn(
+                    `  rotate-[2deg] bg-primary w-[300px] h-[150px] border-2 border-neutral-500 rounded-md flex flex-col justify-center items-center p-4`,
+                    classes
+                )}
+                style={{
+                    rotate: `${rotationDirection.current}${rotationAngle.current}deg`,
+                }}
+            >
+                <div className='w-full relative z-0 '>
+                    <span
+                        ref={wordSpan}
+                        className='z-0  select-none text-4xl text-white hyphens-auto text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full'
+                    >
+                        {word}
+                    </span>
+                    <span
+                        ref={translationSpan}
+                        className='z-0 temporarilyHidden select-none text-4xl text-white  hyphens-auto text-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full'
+                    >
+                        {translation}
+                    </span>
+                </div>
+            </div>
+        </>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,181 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import Card from "./components/Card";
+import { Check, Eye, EyeOff, X } from "lucide-react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    const rememberBtn = useRef<HTMLButtonElement>(null);
+    const forgetBtn = useRef<HTMLButtonElement>(null);
+    const topWord = useRef<HTMLDivElement>(null);
+    const [vocabulary, setVocabulary] = useState<
+        {
+            word: string;
+            translation: string;
+        }[]
+    >([
+        {
+            word: "handkerchief",
+            translation: "носовой платок",
+        },
+        {
+            word: "umbrella",
+            translation: "зонт",
+        },
+        {
+            word: "pencil",
+            translation: "карандаш",
+        },
+        {
+            word: "book",
+            translation: "книга",
+        },
+        {
+            word: "table",
+            translation: "стол",
+        },
+    ]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    const [isTranslation, setIsTranslation] = useState(false);
+
+    useEffect(() => {
+        const wordCards = document.querySelectorAll(".wordCard");
+        console.log(wordCards);
+
+        forgetBtn.current?.addEventListener("touchstart", () => {
+            topWord.current?.classList.add("onTouchForgot");
+            console.log(topWord.current);
+        });
+        forgetBtn.current?.addEventListener("touchend", () => {
+            topWord.current?.classList.remove("onTouchForgot");
+            console.log(topWord.current);
+        });
+        rememberBtn.current?.addEventListener("touchstart", () => {
+            topWord.current?.classList.add("onTouchRemember");
+            console.log(topWord.current);
+        });
+        rememberBtn.current?.addEventListener("touchend", () => {
+            topWord.current?.classList.remove("onTouchRemember");
+            console.log(topWord.current);
+        });
+
+        rememberBtn.current?.addEventListener("click", () => {
+            const fadeAnimation = topWord.current?.animate(
+                [
+                    {
+                        opacity: 1,
+                        transform:
+                            "translateX(-50%) rotate(-20deg) translateY(24px)",
+                    },
+                    {
+                        opacity: 0,
+                        transform: "translateX(-120%) rotate(-40deg) ",
+                    },
+                    {
+                        opacity: 0,
+                        transform: "translateX(-200%) rotate(40deg) ",
+                    },
+                ],
+                {
+                    duration: 300,
+                }
+            );
+            fadeAnimation!.onfinish = () => {
+                topWord.current!.style.opacity = "0";
+                setVocabulary((prev) => {
+                    console.log(prev);
+
+                    const newVocabulary = [...prev.slice(0, prev.length - 1)];
+                    console.log(newVocabulary);
+
+                    return newVocabulary;
+                });
+            };
+        });
+
+        forgetBtn.current?.addEventListener("click", () => {
+            const fadeAnimation = topWord.current!.animate(
+                [
+                    {
+                        opacity: 1,
+                        transform:
+                            "translateX(50%) rotate(20deg) translateY(24px)",
+                    },
+                    {
+                        opacity: 0,
+                        transform: "translateX(120%) rotate(40deg) ",
+                    },
+
+                    {
+                        opacity: 0,
+                        transform: "translateX(200%) rotate(40deg) ",
+                    },
+                ],
+                {
+                    duration: 300,
+                }
+            );
+            fadeAnimation.onfinish = () => {
+                topWord.current!.style.opacity = "0";
+                setVocabulary((prev) => {
+                    const newVocabulary = [...prev.slice(0, prev.length - 1)];
+                    console.log(newVocabulary);
+
+                    return newVocabulary;
+                });
+            };
+        });
+    }, []);
+
+    return (
+        <>
+            <div className='w-screen h-screen bg-primary flex justify-center items-center relative overflow-hidden '>
+                <button
+                    ref={rememberBtn}
+                    className='peer/accept border-2 rounded-full size-16 border-white bottom-1/4 left-[calc(50%-32px-64px-16px)] absolute flex items-center justify-center'
+                >
+                    <Check className='text-white size-8' />
+                </button>
+                <button
+                    onClick={() => {
+                        setIsTranslation((prev) => !prev);
+                    }}
+                    className='border-2 rounded-full size-16 border-white bottom-1/4 left-[calc(50%-32px)] absolute flex items-center justify-center'
+                >
+                    {!isTranslation ? (
+                        <EyeOff className='text-white size-8' />
+                    ) : (
+                        <Eye className='text-white size-8' />
+                    )}
+                </button>
+                <button
+                    ref={forgetBtn}
+                    className='peer/deny border-2 rounded-full size-16 border-white bottom-1/4 left-[calc(50%-32px+64px+16px)] absolute flex items-center justify-center'
+                >
+                    <X className='text-white size-8' />
+                </button>
+                {vocabulary.map((item, index) => (
+                    <div
+                        key={index}
+                        ref={index === vocabulary.length - 1 ? topWord : null}
+                        className='wordCard group absolute flex flex-col gap-2 left-1/2 -translate-x-1/2 not-last:brightness-80 last:peer-hover/accept:-translate-x-full last:peer-hover/accept:translate-y-6 last:peer-hover/accept:rotate-[-20deg] last:peer-hover/deny:translate-x-0 last:peer-hover/deny:translate-y-6 last:peer-hover/deny:rotate-[20deg] transition-all duration-300'
+                    >
+                        {/* <span className='[.onTouchRemember_&]:opacity-100 opacity-0 text-neutral-400 font-semibold transition-all duration-300 group-last:block hidden'> */}
+                        {/* <span className='[.onTouchForgot_&]:block hidden'>
+                                I DON&apos;T remember this word
+                            </span>
+                            <span className='[.onTouchRemember_&]:block hidden'>
+                                I DO remember this word
+                            </span> */}
+                        {/* </span> */}
+                        <Card
+                            word={item.word}
+                            translation={item.translation}
+                            isTranslation={isTranslation}
+                        />
+                    </div>
+                ))}
+            </div>
+        </>
+    );
 }

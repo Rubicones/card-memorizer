323714a37539a7ead531c7e99705592d843b34da
Rubicones <dmitriypopov1543@gmail.com>
2025-08-10 23:31:40 +0200

    favicon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import { ReduxProvider } from "@/lib/store/provider";
-import { Toaster } from "@/components/ui/sonner"
-
+import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
+import Head from "next/head";
 
 const montserrat = Montserrat({
     variable: "--font-montserrat",
@@ -22,6 +22,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang='en' className='dark'>
+            <Head>
+                <link rel='icon' href='/favicon.svg' sizes='any' />
+            </Head>
             <body className={`${montserrat.variable} antialiased`}>
                 <ReduxProvider>{children}</ReduxProvider>
                 <Toaster />
